7d01e8e6eaa0f7d6d4da9feff33f3b57c8d742be
iJungleboy <daniel.mettler@2sic.com>
2018-04-13 17:17:18 +0200

    simplify distribution / updating of 2sxc package; copy dist directly like with other parts...

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+﻿(() => {
+  var gulp = require('gulp'),
+    finalDest = './../2sxc-dnn742/Website' + '/DesktopModules/ToSIC_SexyContent/dist';
+
+
+  // deploy to the current 2sxc-dev
+  gulp.task('copy-dist-to-2sxc', function () {
+    gulp.src(['./dist/**/*'])
+      .pipe(gulp.dest(finalDest));
+  });
+
+  gulp.task('watch-dist', function () {
+    gulp.watch('./dist/**/*', ['copy-dist-to-2sxc']);
+  });
+
+
+})();
